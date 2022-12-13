aea38cde55ba0c2bd25a36b63430465181356332
tignioj <859897994@qq.com>
2022-12-13 21:23:04 +0800

    删除无用代码

--- src/ArduinoUZlib.cpp
+++ src/ArduinoUZlib.cpp
@@ -1,10 +1,7 @@
 #include "ArduinoUZlib.h"
 #define OUT_CHUNK_SIZE 1
 ArduinoUZlib::ArduinoUZlib(){}
-static void error(String msg) {
-  Serial.print("!!!ERROR:");
-  Serial.println(msg);
-}
+
 void exit_error(const char *what)
 {
     printf("ERROR: %s\n", what);
@@ -64,54 +61,4 @@ int32_t ArduinoUZlib::decompress(uint8_t *in_buf, uint32_t in_size, uint8_t *&de
 	out_size= d.dest - dest;
 	return  out_size;
 
-
-
-
-
-// 	/* some common variables. */
-// 	int32_t result = 0;
-// 	int32_t tb     = 0;
-//   	outprintsize=0;
-// 	z_stream z;
-
-// 	/* fill the stream structure for zlib. */
-// 	z.next_in   = (Bytef *)in_buf;
-// 	z.avail_in  = (uInt)in_size;
-// 	z.total_in  = in_size;
-// 	z.next_out  = (Bytef *)out_buf;
-// 	z.avail_out = (uInt)out_size;
-// 	z.total_out = 0;
-// 	z.zalloc    = NULL;
-// 	z.zfree     = NULL;
-  
-//   if((result=inflateInit2(&z, MAX_WBITS|32)) != Z_OK) {
-  
-//   // if((result=inflateInit2(&z, 47)) != Z_OK) {
-//     error("something on zlib initialization failed.");
-//       return result;
-//   }
-
-// 	/* call zlib to decompress the data. */
-// 	if ((result = inflate(&z, Z_FINISH)) != Z_STREAM_END) {
-//     error("inflate something on zlib decompression failed");
-//     Serial.println(result);
-// 		/* something on zlib decompression failed. */
-// 		return result;
-// 	}
-
-// 	/* save transferred bytes. */
-// 	tb = z.total_out;
-
-// 	/* cleanup zlib. */
-// 	if ((result = inflateEnd(&z)) != Z_OK) {
-//     error("something on zlib finalization failed.");
-// 		/* something on zlib finalization failed. */
-// 		return result;
-// 	}
-
-// 	/* return transferred bytes. */
-//   Serial.println("decompress success!");
-//     outprintsize=tb;
-//     Serial.println(tb);
-// 	return tb;
 } 
